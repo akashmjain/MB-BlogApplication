3950191e61efe207fa42d3237c91c3316673077a
Akash Jain <business@akashmjain.com>
2021-02-02 12:42:53 +0530

    show blog added

--- src/main/java/com/akashmjain/BlogApplication/controller/BlogHomeController.java
+++ src/main/java/com/akashmjain/BlogApplication/controller/BlogHomeController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogHomeController {
@@ -19,5 +20,11 @@ public class BlogHomeController {
     public String getFrontPageData(Model model) {
         model.addAttribute("posts", blogService.getBlogPostsForFrontPage());
         return "index";
+    }
+
+    @RequestMapping("/blog")
+    public String getIndividualBlog(@RequestParam("id") int blogId, Model model) {
+        model.addAttribute("blog", blogService.getIndividualBlogPost(blogId));
+        return "show_blog";
     }
 }

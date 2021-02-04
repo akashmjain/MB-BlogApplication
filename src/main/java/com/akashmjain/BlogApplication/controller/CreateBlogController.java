b426a76aaf2d09d59c5cfbf232b6b8cb671beaee
Akash Jain <business@akashmjain.com>
2021-02-04 20:04:28 +0530

    tags parsing remaining

--- src/main/java/com/akashmjain/BlogApplication/controller/CreateBlogController.java
+++ src/main/java/com/akashmjain/BlogApplication/controller/CreateBlogController.java
@@ -1,6 +1,7 @@
 package com.akashmjain.BlogApplication.controller;
 
 import com.akashmjain.BlogApplication.enitity.PostEntity;
+import com.akashmjain.BlogApplication.enitity.TagEntity;
 import com.akashmjain.BlogApplication.service.post.PostService;
 import com.akashmjain.BlogApplication.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 @Controller
 @RequestMapping("/create")
@@ -22,20 +27,31 @@ public class CreateBlogController {
     public String createNewPost(Model model) {
         PostEntity postEntity = new PostEntity();
         model.addAttribute("post", postEntity);
+        model.addAttribute("tagStringData", "");
         return "write_blog";
     }
 
     @PostMapping("/save")
-    public String savePost(@ModelAttribute("post") PostEntity post) {
+    public String savePost(@ModelAttribute("post") PostEntity post, @RequestParam("tag_string_data") String tags) {
+        //@TODO parse the tags and craete new tags if needed and assign those to Post;
         post.setAuthor(userService.findById(1));
         post.setPublished(true);
-        post.setExcerpt("basic excerpt");
+        post.setPublished(true);
+        post.setPublishedAt(new Timestamp(System.currentTimeMillis()));
+        post.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
         postService.save(post);
         return "redirect:/";
     }
+    /*
+    private List<TagEntity> createTags(String tagLine) {
+        String [] tagArr = tagLine.split(" ");
+        return null;
+    }*/
 
     @RequestMapping("/updatePost")
-    public String updatePost() {
-        return null;
+    public String updatePost(@ModelAttribute("post") PostEntity post) {
+        postService.save(post);
+        return "redirect:/blog?id="+post.getId();
     }
+
 }
